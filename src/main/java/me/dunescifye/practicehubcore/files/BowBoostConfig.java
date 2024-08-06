b907c8d4a21462b8e2de2fc6820b7237f4db214a
DuneSciFye <dunescifye@gmail.com>
2024-08-05 21:06:30 -0400

    Added BowBoost gamemode

--- src/main/java/me/dunescifye/practicehubcore/files/BowBoostConfig.java
+++ src/main/java/me/dunescifye/practicehubcore/files/BowBoostConfig.java
@@ -2,6 +2,9 @@ package me.dunescifye.practicehubcore.files;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
 import me.dunescifye.practicehubcore.PracticeHubCore;
+import me.dunescifye.practicehubcore.gamemodes.bowboost.BowBoost;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +17,14 @@ public class BowBoostConfig {
 
         try {
             YamlDocument config = YamlDocument.create(new File(plugin.getDataFolder(), "gamemodes/BowBoost/BowBoost.yml"), plugin.getResource("gamemodes/BowBoost/BowBoost.yml"));
+            //Copy World
+            String worldName = config.getString("CopyWorld");
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                logger.severe("World \"" + worldName + "\" not found! Bridge gamemode disabled until fixed.");
+            } else {
+                BowBoost.bowBoostCopyWorld = worldName;
+            }
         } catch (
             IOException e) {
             logger.severe("Failed to load file gamemodes/BowBoost.yml");
--- src/main/java/me/dunescifye/practicehubcore/gamemodes/bowboost/BowBoost.java
+++ src/main/java/me/dunescifye/practicehubcore/gamemodes/bowboost/BowBoost.java
@@ -1,4 +1,50 @@
 package me.dunescifye.practicehubcore.gamemodes.bowboost;
 
+import me.dunescifye.practicehubcore.PracticeHubCore;
+import me.dunescifye.practicehubcore.gamemodes.PracticeHubPlayer;
+import net.kyori.adventure.text.Component;
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class BowBoost {
+
+    public static String bowBoostCopyWorld = null;
+
+    public static void startBowBoostGame(Player p) {
+        PracticeHubPlayer player = new PracticeHubPlayer();
+
+        //Setting up world
+        PracticeHubCore.worldManager.cloneWorld(bowBoostCopyWorld, "bowBoost" + p.getName());
+
+        //Teleport player
+        World world = Bukkit.getWorld("bowBoost" + p.getName());
+        if (world == null) {
+            p.sendMessage(Component.text("Invalid world. Please contact an administrator."));
+            return;
+        }
+        p.teleport(new Location(world, 0, 100, 0));
+        p.setGameMode(GameMode.SURVIVAL);
+        p.setFoodLevel(20);
+
+        //Setting up inventory
+        player.setSavedInventory(p.getInventory().getContents());
+        Inventory inv = p.getInventory();
+        inv.clear();
+        ItemStack bow = new ItemStack(Material.BOW);
+        bow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
+        ItemMeta meta = bow.getItemMeta();
+        meta.setUnbreakable(true);
+        bow.setItemMeta(meta);
+        inv.setItem(0, bow);
+
+        //Everything works
+        p.sendMessage(Component.text("Starting!"));
+        player.setGamemode("BowBoost");
+
+    }
+
 }
