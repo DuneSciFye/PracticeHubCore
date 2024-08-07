ebdfd7c3ae3b21223423b483b1540dc1a8c484b6
DuneSciFye <dunescifye@gmail.com>
2024-08-07 18:19:21 -0400

    Work on Fall Clutch gamemode

--- src/main/java/me/dunescifye/practicehubcore/commands/FallClutchCommand.java
+++ src/main/java/me/dunescifye/practicehubcore/commands/FallClutchCommand.java
@@ -3,6 +3,8 @@ package me.dunescifye.practicehubcore.commands;
 import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.LiteralArgument;
 import me.dunescifye.practicehubcore.gamemodes.FallClutch;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class FallClutchCommand {
 
@@ -12,12 +14,24 @@ public class FallClutchCommand {
                 .then(new LiteralArgument("random"))
                 .then(new LiteralArgument("waterbucket")
                     .executesPlayer((p, args) -> {
-                        FallClutch.startGame(p);
+                        FallClutch.startGame(p, new ItemStack(Material.WATER_BUCKET));
                     })
                 )
                 .then(new LiteralArgument("boat")
+                    .executesPlayer((p, args) -> {
+                        FallClutch.startGame(p, new ItemStack(Material.OAK_BOAT));
+                    })
                 )
-                .then(new LiteralArgument("scaffolding"))
+                .then(new LiteralArgument("scaffolding")
+                    .executesPlayer((p, args) -> {
+                        FallClutch.startGame(p, new ItemStack(Material.SCAFFOLDING));
+                    })
+                )
+                .then(new LiteralArgument("berry")
+                    .executesPlayer((p, args) -> {
+                        FallClutch.startGame(p, new ItemStack(Material.SWEET_BERRIES));
+                    })
+                )
             )
             .withPermission("practicehub.command.fallclutch")
             .register("practicehub");
--- src/main/java/me/dunescifye/practicehubcore/gamemodes/FallClutch.java
+++ src/main/java/me/dunescifye/practicehubcore/gamemodes/FallClutch.java
@@ -5,23 +5,31 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class FallClutch {
+public class FallClutch implements Listener {
 
     public static World world;
     public static List<Location> grid;
     public static int gridSpacing = 500;
 
-    public static void startGame(Player p) {
+    public static void startGame(Player p, ItemStack item) {
         PracticeHubPlayer player = new PracticeHubPlayer(p);
         Location spawnLocation = new Location(world, 0, ThreadLocalRandom.current().nextInt(-30, 250), 0);
         while (grid.contains(spawnLocation)) {
             spawnLocation.add(gridSpacing, 0, 0);
         }
+
+        //Give item
+        player.saveInventory(item);
 
         p.teleport(spawnLocation);
 
@@ -36,12 +44,24 @@ public class FallClutch {
                 if (confirm > 20) {
                     cancel();
                     p.sendMessage(Component.text("You win!"));
+                    player.retrieveInventory();
                 }
             }
         }.runTaskTimer(PracticeHubCore.getPlugin(), 0L, 2L);
 
-
+        player.setGamemode("FallClutch");
         PracticeHubPlayer.linkedPlayers.put(p, player);
+    }
+
+    @EventHandler
+    public void onPlayerTakeFallDamage(EntityDamageEvent e) {
+        if (!(e.getEntity() instanceof Player p)) return;
+        if (e.getCause() != EntityDamageEvent.DamageCause.FALL) return;
+        PracticeHubPlayer player = PracticeHubPlayer.linkedPlayers.get(p);
+        if (player == null || !Objects.equals(player.getGamemode(), "FallClutch")) return;
+
+        e.setCancelled(true);
+        p.sendMessage("Failed!");
     }
 
 }
