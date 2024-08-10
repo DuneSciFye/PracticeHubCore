e29b88e58b368bc4917158c06262ce37c1fa6b41
DuneSciFye <dunescifye@gmail.com>
2024-08-09 20:33:08 -0400

    Work on Parkour Gamemode

--- src/main/java/me/dunescifye/practicehubcore/commands/ParkourCommand.java
+++ src/main/java/me/dunescifye/practicehubcore/commands/ParkourCommand.java
@@ -2,6 +2,7 @@ package me.dunescifye.practicehubcore.commands;
 
 import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.LiteralArgument;
+import me.dunescifye.practicehubcore.gamemodes.Parkour;
 
 public class ParkourCommand {
 
@@ -10,9 +11,29 @@ public class ParkourCommand {
             .then(new LiteralArgument("start")
                 .then(new LiteralArgument("random")
                     .executesPlayer((p, args) -> {
-
+                        Parkour.startGame(p, "random");
                     })
                 )
+                .then(new LiteralArgument("easy")
+                    .executesPlayer((p, args) -> {
+                        Parkour.startGame(p, "random");
+                    })
+                )
+                .then(new LiteralArgument("medium")
+                    .executesPlayer((p, args) -> {
+                        Parkour.startGame(p, "random");
+                    })
+                )
+                .then(new LiteralArgument("hard")
+                    .executesPlayer((p, args) -> {
+                        Parkour.startGame(p, "random");
+                    })
+                )
+            )
+            .then(new LiteralArgument("end")
+                .executesPlayer((p, args) -> {
+                    Parkour.endGame(p);
+                })
             )
             .withPermission("practicehub.command.parkour")
             .register("practicehub");
--- src/main/java/me/dunescifye/practicehubcore/files/ParkourConfig.java
+++ src/main/java/me/dunescifye/practicehubcore/files/ParkourConfig.java
@@ -1,4 +1,38 @@
 package me.dunescifye.practicehubcore.files;
 
+import dev.dejvokep.boostedyaml.YamlDocument;
+import me.dunescifye.practicehubcore.PracticeHubCore;
+import me.dunescifye.practicehubcore.gamemodes.LilyPadBridge;
+import me.dunescifye.practicehubcore.gamemodes.Parkour;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class ParkourConfig {
+    public static void setup() {
+        PracticeHubCore plugin = PracticeHubCore.getPlugin();
+        Logger logger = plugin.getLogger();
+
+        try {
+            YamlDocument config = YamlDocument.create(new File(plugin.getDataFolder(), "gamemodes/Parkour/Parkour.yml"), plugin.getResource("gamemodes/Parkour/Parkour.yml"));
+            //Copy World
+            String worldName = config.getString("CopyWorld");
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                logger.severe("World \"" + worldName + "\" not found! Parkour gamemode disabled until fixed.");
+                return;
+            }
+            Parkour.worldName = worldName;
+            String[] coords = config.getString("SpawnLocation").split(" ");
+            Parkour.spawnLocation = Arrays.stream(coords).mapToInt(Integer::parseInt).toArray();
+        } catch (
+            IOException e) {
+            logger.severe("Failed to load file gamemodes/Parkour/Parkour.yml");
+            throw new RuntimeException(e);
+        }
+    }
 }
